7d099cda8043047d32e6a9f9e98239c7dad2a32e
gxf <740297955@qq.com>
2019-12-04 20:55:19 +0800

    3

--- git.sh
+++ git.sh
@@ -5,4 +5,15 @@ git add . #将当前修改添加至缓存区
 git commit -m '1' #提交
 git push #推送
 
-在github 创建仓库的时候，如果勾选添加readme文件后，在本地需要先执行 git pull 远程仓库地址 master ，确保远程仓库和本地一致
+#在github 创建仓库的时候，如果勾选添加readme文件后，在本地需要先执行 git pull 远程仓库地址 master ，确保远程仓库和本地一致
+
+#后面使用的时候只需
+
+#选做
+git status #查看修改的文件
+git diff #查看具体修改的内容
+
+#必须做
+git add . #将当前修改添加至缓存区
+git commit -m '1' 
+git push #推送
