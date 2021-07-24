d0ad88b5785fe686ac5a579c1019d9d06408b7e2
Shelfcol <740297955@qq.com>
2021-07-24 14:14:39 +0800

    modiffy catkin_ws build

--- shell/build_ros_workspace.sh
+++ shell/build_ros_workspace.sh
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-mkdir -p ~/catkin_ws_nio/src
-cd ~/catkin_ws_nio/src
+mkdir -p ~/catkin_ws_cs/src
+cd ~/catkin_ws_cs/src
 catkin_init_workspace
-cd ~/catkin_ws_nio/
-=======
-mkdir -p ~/catkin_ws_boonrayCar/src
-cd ~/catkin_ws_boonrayCar/src
-catkin_init_workspace
-cd ~/catkin_ws_boonrayCar/
->>>>>>> faf225b023762212a50820bbedab76f84a118dff
+cd ~/catkin_ws_cs/
 catkin_make
